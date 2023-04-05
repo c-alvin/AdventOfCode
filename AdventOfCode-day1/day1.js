13428161afa186500fcc55a5ded553a64873a9f1
Alvin Cho <alvinxcho2@gmail.com>
2023-04-05 08:41:08 +0000

    Completed AdventOfCode-day1-part2

--- AdventOfCode-day1/day1.js
+++ AdventOfCode-day1/day1.js
@@ -3,29 +3,45 @@ const fs = require('fs');
 let inputData = '';
 const elfArray = [];
 let largestElf = -Infinity;
+let secondLargestElf = 0;
+let thirdLargestElf = 0;
+let totalCalories = 0;
 
 fs.readFile('./input.txt', 'utf8', (err, data) => {
   if (err) throw Error;
   inputData = data.toString();
   const initialValue = 0;
-  const stringData = inputData
-    .split('\n')
-    .reduce((accumulator, currentValue) => {
-      if (currentValue === '') {
-        elfArray.push(accumulator);
-        currentValue = 0;
-        accumulator = 0;
-      }
+  inputData.split('\n').reduce((accumulator, currentValue) => {
+    if (currentValue === '') {
+      elfArray.push(accumulator);
+      currentValue = 0;
+      accumulator = 0;
+    }
 
-      return parseInt(accumulator) + parseInt(currentValue);
-    }, initialValue);
+    return parseInt(accumulator) + parseInt(currentValue);
+  }, initialValue);
 
   for (let i = 0; i < elfArray.length; i++) {
     if (elfArray[i] > largestElf) {
       largestElf = elfArray[i];
     }
   }
-  console.log(largestElf);
-  console.log(stringData);
-  return largestElf;
+  elfArray.sort(function (a, b) {
+    return b - a;
+  });
+
+  for (let i = 0; i < 3; i++) {
+    if (i === 0) {
+      largestElf = elfArray[i];
+    }
+    if (i === 1) {
+      secondLargestElf = elfArray[i];
+    }
+    if (i === 2) {
+      thirdLargestElf = elfArray[i];
+    }
+  }
+
+  totalCalories = largestElf + secondLargestElf + thirdLargestElf;
+  console.log(totalCalories);
 });
